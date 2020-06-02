e0095796830236e05b52871c83157ccdc9851df6
vagrant <vagrant@vagrant.vm>
2020-06-02 15:49:50 +0000

    feat: add kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
-
+dockerpath=subrockmann/udacity_microservice:v1
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+sudo kubectl run my-app --image=subrockmann/udacity_microservice:v1 --port=80
 
 # Step 3:
 # List kubernetes pods
+sudo kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+echo "Please wait for 10 seconds for pod to come up"
+sleep 10
+sudo kubectl port-forward pod/my-app 8080:80
+sudo kubectl logs --selector app=my-app
 
